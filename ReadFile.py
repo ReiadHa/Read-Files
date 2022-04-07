6d097c43b3f0887cff51360171fa0f597fc92445
Riad Hak <99054420@mydavinci.nl>
2022-04-07 09:58:24 +0200

    update

--- ReadFile.py
+++ ReadFile.py
@@ -1,6 +1,7 @@
 import time
-file = open('C:/Users/redah/OneDrive - Da Vinci College/A Mbo4/Git/Read-Files/README.md', 'r')
 
-for line in file:
-    time.sleep(1)
-    print(line)
+with open('C:/Users/redah/OneDrive - Da Vinci College/A Mbo4/Git/Read-Files/README.md', 'r') as file:
+    for line in file:
+        print(line)
+        time.sleep(1)
+
